a87d93f2a996e41c4012b4595d3c6567990eecb0
gto85 <gto85@mail.ru>
2024-01-14 05:17:50 +0700

    1

---
+++ bot/models/user.model.js
@@ -0,0 +1,27 @@
+const { db } = require("../connections/db.connection");
+const { DataTypes } = require("sequelize");
+
+module.exports = db.define("user", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+    },
+    login: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    privileged: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+    }
+},{
+       timestamps: true,
+       updateAt: false,
+});
