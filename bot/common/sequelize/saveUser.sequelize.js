11c2a4ac4af3ddca5e08b049c63d697cc59e16dc
gto85 <gto85@mail.ru>
2024-01-14 10:41:50 +0700

    1

---
+++ bot/common/sequelize/saveUser.sequelize.js
@@ -0,0 +1,22 @@
+const db = require('../../connections/db.connection');
+const UserModel = require('../../models/user.model');
+
+exports.saveUser = async (login, username) => {
+
+    await db.sync();
+
+    const successMessage = `User ${login}-${username} is saved in the database.`;
+    const updateMessage = `User ${login}-${username} has been updated in the database.`;
+
+    const foundUser = await UserModel.findOne({ where: { login } });
+
+    if(!foundUser) {
+        await UserModel.create({ login, username });
+        return successMessage;
+    }
+    if(foundUser.username !== username) {
+        await UserModel.update({  username }, {where: { login} });
+
+    }
+    return updateMessage;
+};
